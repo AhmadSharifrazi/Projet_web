6766be07ea115a42940eed6625d0b9ba3553028c
AhmadSharifrazi <38720924+AhmadSharifrazi@users.noreply.github.com>
2018-05-02 11:07:26 +0200

    Delete Footer.php

--- Views/Footer.php
+++
@@ -1,6 +0,0 @@
-		<footer>
-			<p id="copyright">développé par <?php echo DEV1?> et <?php echo DEV2?></p>
-			<p id="contact">Runsmart: +32<?php echo NUM_TEL ?> - <br> <?php echo EMAIL?></p>
-		</footer>
-	</body>
-</html>
